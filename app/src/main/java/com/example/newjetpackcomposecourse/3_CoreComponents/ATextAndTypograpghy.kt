31f3e8b32bbdc8d26eed1b20320642e49d9e229e
Mohdnabee <mohdnabeel989@gmail.com>
2025-08-05 23:40:18 +0530

    core concepts

--- app/src/main/java/com/example/newjetpackcomposecourse/3_CoreComponents/ATextAndTypograpghy.kt
+++ app/src/main/java/com/example/newjetpackcomposecourse/3_CoreComponents/ATextAndTypograpghy.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -20,9 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
-import com.example.newjetpackcomposecourse.R
 
 @Composable
 fun SimpleText(){
