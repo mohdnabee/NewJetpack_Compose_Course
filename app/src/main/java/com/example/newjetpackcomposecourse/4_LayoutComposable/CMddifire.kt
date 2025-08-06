e42fc8e12def4d7ab90c033ac545d2e713f6dcfb
Mohdnabee <mohdnabeel989@gmail.com>
2025-08-06 20:51:50 +0530

    layout  composable

---
+++ app/src/main/java/com/example/newjetpackcomposecourse/4_LayoutComposable/CMddifire.kt
@@ -0,0 +1,24 @@
+package com.example.newjetpackcomposecourse.`4_LayoutComposable`
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+//  Modifiers
+//  Modifiers are available in all  the composable like Text, Button , Image ,Column etc
+
+@Composable
+fun ModifierExample() {
+
+
+    Box(
+        modifier = Modifier
+    ){
+
+    }
+
+
+}
+
+//  4:43:44
+//  $;$#;$$
