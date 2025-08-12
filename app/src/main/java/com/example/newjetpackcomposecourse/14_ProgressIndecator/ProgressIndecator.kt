0c2e756026265c095c26dccbb5c3ea0e885ee820
Mohdnabee <mohdnabeel989@gmail.com>
2025-08-12 08:48:19 +0530

    progress Indecator

---
+++ app/src/main/java/com/example/newjetpackcomposecourse/14_ProgressIndecator/ProgressIndecator.kt
@@ -0,0 +1,61 @@
+package com.example.newjetpackcomposecourse.`14_ProgressIndecator`
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularIndicatorExample() {
+
+    var loading by remember {
+        mutableStateOf(false)
+    }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                loading = true
+            }, enabled = !loading
+
+        ) { Text("Start Loading") }
+
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        if (!loading) return CircularProgressIndicator(
+            modifier = Modifier.width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun CircularIndicatorPreview() {
+    CircularIndicatorExample()
+}
